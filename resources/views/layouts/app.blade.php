697071df73e2dfbf14b76c4a2fe8794b03d04470
hase07180 <yuukisoccer07180@gmail.com>
2022-04-25 00:04:02 +0900

    レスポンシブ対応
    Bootstrapのグリッドシステムを使用して簡単にスマホ対応を行なった。

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
         {{-- 3カラムに変更 --}}
         <main class="">
             <div class="row">
-                <div class="col-md-2 p-0">
+                <div class="col-sm-12 col-md-2 p-0">
                     <div class="card">
                         <div class="card-header">タグ一覧</div>
                         <div class="card-body my-card-body">
@@ -97,7 +97,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-4 p-0">
+                <div class="col-sm-12 col-md-4 p-0">
                     <div class="card">
                         <div class="card-header d-flex justify-content-between">メモ一覧 <a href="{{ route('home') }}"><i
                                     class="fas fa-plus-circle"></i></a></div>
@@ -110,7 +110,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-6 p-0">
+                <div class="col-sm-12 col-md-6 p-0">
                     @yield('content')
                 </div>
             </div>
